dc07d3c0fe8b278242dd801bd0fea784dcc20ed0
Flavio Roso <flaviorosogoncalves1@gmail.com>
2020-09-10 22:24:40 +0000

    Subir build para futuro webhook

---
+++ build/precache-manifest.8434e77d9c4b8c9f431101d548d7ecf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93a677c383b5c97dbd5db83dfb4a704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10ec355b32bae6f0e213",
+    "url": "/static/js/2.7fdf4cd5.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.7fdf4cd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff25ffb01c21ecb282e5",
+    "url": "/static/js/main.30bea15c.chunk.js"
+  },
+  {
+    "revision": "7fdbed021e3a5eaed5ef",
+    "url": "/static/js/runtime-main.ab785f0e.js"
+  }
+]);
